b5c18f9e92bcb77f3741f26c62c7f4835d4952c8
jyu6 <juanlu_yu@intuit.com>
2022-09-14 16:16:03 -0700

    add TODO
    Signed-off-by: jyu6 <juanlu_yu@intuit.com>

--- pynumaflow/_constants.py
+++ pynumaflow/_constants.py
@@ -4,4 +4,6 @@ NUMAFLOW_UDF_CONTENT_TYPE = "NUMAFLOW_UDF_CONTENT_TYPE"
 NUMAFLOW_MESSAGE_KEY = "x-numa-message-key"
 FUNCTION_SOCK_PATH = "/var/run/numaflow/udf.sock"
 SINK_SOCK_PATH = "/var/run/numaflow/udsink.sock"
+# TODO: need to make sure the DATUM_KEY value is the same as
+# https://github.com/numaproj/numaflow-go/blob/main/pkg/function/configs.go#L6
 DATUM_KEY = "x-numaflow-datum-key"

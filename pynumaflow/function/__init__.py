764ee62a7e3e9343839e733cd9eab778cd8a155f
jyu6 <juanlu_yu@intuit.com>
2022-09-14 16:26:27 -0700

    update function __init__.py
    Signed-off-by: jyu6 <juanlu_yu@intuit.com>

--- pynumaflow/function/__init__.py
+++ pynumaflow/function/__init__.py
@@ -1,5 +1,4 @@
 from pynumaflow.function._dtypes import Message, Messages
-from pynumaflow.datum import Datum
 from pynumaflow.function.server import UserDefinedFunctionServicer
 
 __all__ = ["Message", "Messages", "UserDefinedFunctionServicer"]

baebbda668a4499c7b9feba7e3a46e34c58b1728
Shrivardhan Rao <Shrivardhan_Rao@intuit.com>
2022-09-29 17:58:21 -0700

    update function socket

--- pynumaflow/_constants.py
+++ pynumaflow/_constants.py
@@ -2,7 +2,7 @@ APPLICATION_JSON = "application/json"
 APPLICATION_MSG_PACK = "application/msgpack"
 NUMAFLOW_UDF_CONTENT_TYPE = "NUMAFLOW_UDF_CONTENT_TYPE"
 NUMAFLOW_MESSAGE_KEY = "x-numa-message-key"
-FUNCTION_SOCK_PATH = "/var/run/numaflow/udf.sock"
+FUNCTION_SOCK_PATH = "/var/run/numaflow/function.sock"
 SINK_SOCK_PATH = "/var/run/numaflow/udsink.sock"
 # TODO: need to make sure the DATUM_KEY value is the same as
 # https://github.com/numaproj/numaflow-go/blob/main/pkg/function/configs.go#L6

c2109a067f73e1a0abe4a333aeec016d4a5a5662
Sidhant Kohli <sidhant.kohli@gmail.com>
2023-03-04 14:49:22 -0800

    docs: examples for asyncio with ThreadPoolExecutor and ProcessPoolExecutor (#61)
    Signed-off-by: Sidhant Kohli <sidhant.kohli@gmail.com>
    Signed-off-by: Vigith Maurice <vigith@gmail.com>
    Signed-off-by: jyu6 <juanlu_yu@intuit.com>
    Co-authored-by: Vigith Maurice <vigith@gmail.com>
    Co-authored-by: jyu6 <juanlu_yu@intuit.com>

---
+++ examples/function/asyncio-reduce/async_exec.py
@@ -0,0 +1,52 @@
+import asyncio
+import json
+from typing import AsyncIterable
+
+import aiorun
+import aiohttp
+from pynumaflow import setup_logging
+import time
+
+from pynumaflow.function import (
+    Messages,
+    Message,
+    Datum,
+    Metadata,
+    UserDefinedFunctionServicer,
+)
+
+_LOGGER = setup_logging(__name__)
+
+
+async def http_request(session, url):
+    async with session.get(url) as resp:
+        result = await resp.read()
+        try:
+            res_json = json.loads(result)
+            return res_json["message"]
+        except Exception as e:
+            _LOGGER.error("HTTP request error: %s", e)
+            return "Error"
+
+
+async def reduce_handler(key: str, datums: AsyncIterable[Datum], md: Metadata) -> Messages:
+    interval_window = md.interval_window
+    async with aiohttp.ClientSession() as session:
+        tasks = []
+        start_time = time.time()
+        async for _ in datums:
+            url = f"http://host.docker.internal:9888/ping"
+            tasks.append(http_request(session, url))
+        results = await asyncio.gather(*tasks)
+        end_time = time.time()
+
+    msg = (
+        f"batch_time:{end_time-start_time} interval_window_start:{interval_window.start} "
+        f"interval_window_end:{interval_window.end}"
+    )
+    return Messages(Message.to_vtx(key, str.encode(msg)))
+
+
+if __name__ == "__main__":
+    grpc_server = UserDefinedFunctionServicer(reduce_handler=reduce_handler)
+    aiorun.run(grpc_server.start_async())

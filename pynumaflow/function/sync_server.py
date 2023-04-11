b84cb56f5ab1f5e465ffeeb0c975a4e0a82ac12f
Sidhant Kohli <sidhant.kohli@gmail.com>
2023-04-11 11:33:22 -0700

    sync serv
    Signed-off-by: Sidhant Kohli <sidhant.kohli@gmail.com>

--- pynumaflow/function/sync_server.py
+++ pynumaflow/function/sync_server.py
@@ -202,6 +202,9 @@ class SyncServerServicer(udfunction_pb2_grpc.UserDefinedFunctionServicer):
         The pascal case function name comes from the proto udfunction_pb2_grpc.py file.
         """
         _LOGGER.error("Reduce not supported on SYNC --")
+        context.set_code(grpc.StatusCode.UNIMPLEMENTED)
+        context.set_details("Reduce Not supported")
+        return udfunction_pb2.DatumList()
         raise RuntimeError("Reduce Not supported on sync")
         _LOGGER.error("Reduce not supported on SYNC -- 2")
 

154e84988ed01ae3878bee5e0f8d08c1f309ff10
Yashash H L <yashash_hl@intuit.com>
2023-04-12 12:50:02 +0530

    add conditional forwarding example
    Signed-off-by: Yashash H L <yashash_hl@intuit.com>

---
+++ examples/function/even_odd/example.py
@@ -0,0 +1,28 @@
+from typing import List
+
+from pynumaflow.function import Messages, Message, Datum, UserDefinedFunctionServicer
+
+
+def my_handler(keys: List[str], datum: Datum) -> Messages:
+    val = datum.value
+    output_keys = keys
+    output_tags = []
+    _ = datum.event_time
+    _ = datum.watermark
+    messages = Messages()
+    num = int.from_bytes(val, "little")
+
+    if num % 2 == 0:
+        output_keys = ["even"]
+        output_tags = ["even-tag"]
+    else:
+        output_keys = ["odd"]
+        output_tags = ["odd-tag"]
+
+    messages.append(Message(val, keys=output_keys, tags=output_tags))
+    return messages
+
+
+if __name__ == "__main__":
+    grpc_server = UserDefinedFunctionServicer(map_handler=my_handler)
+    grpc_server.start()

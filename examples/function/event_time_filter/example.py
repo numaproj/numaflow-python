97d7f4efdcc0ae759df2a3d28b7c22253b4f33b8
Keran Yang <yangkr920208@gmail.com>
2023-04-25 23:18:44 -0400

    fix: update event time filter transformer to use tag (#92)
    Signed-off-by: Keran Yang <yangkr920208@gmail.com>

--- examples/function/event_time_filter/example.py
+++ examples/function/event_time_filter/example.py
@@ -8,8 +8,8 @@ from pynumaflow.function import MessageTs, MessageT, Datum, Server
 This is a simple User Defined Function example which receives a message, applies the following
 data transformation, and returns the message.
 If the message event time is before year 2022, drop the message. If it's within year 2022, update
-the key to "within_year_2022" and update the message event time to Jan 1st 2022.
-Otherwise, (exclusively after year 2022), update the key to "after_year_2022" and update the
+the tag to "within_year_2022" and update the message event time to Jan 1st 2022.
+Otherwise, (exclusively after year 2022), update the tag to "after_year_2022" and update the
 message event time to Jan 1st 2023.
 """
 
@@ -31,14 +31,14 @@ def my_handler(keys: List[str], datum: Datum) -> MessageTs:
             event_time,
         )
         messages.append(
-            MessageT(value=val, event_time=january_first_2022, keys=["within_year_2022"])
+            MessageT(value=val, event_time=january_first_2022, tags=["within_year_2022"])
         )
     else:
         logging.info(
             "Got event time:%s, it is after year 2022, so forwarding to after_year_2022", event_time
         )
         messages.append(
-            MessageT(value=val, event_time=january_first_2023, keys=["after_year_2022"])
+            MessageT(value=val, event_time=january_first_2023, tags=["after_year_2022"])
         )
 
     return messages

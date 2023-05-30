0be40b6c8abbc886249c39d7c4d000a223a8f7c9
Hao Hao <xdevxhao@gmail.com>
2023-05-30 10:03:06 -0700

    nits
    Signed-off-by: Hao Hao <xdevxhao@gmail.com>

--- pynumaflow/sink/proto/udsink_pb2.py
+++ pynumaflow/sink/proto/udsink_pb2.py
@@ -6,7 +6,6 @@ from google.protobuf.internal import builder as _builder
 from google.protobuf import descriptor as _descriptor
 from google.protobuf import descriptor_pool as _descriptor_pool
 from google.protobuf import symbol_database as _symbol_database
-
 # @@protoc_insertion_point(imports)
 
 _sym_db = _symbol_database.Default()
@@ -16,26 +15,25 @@ from google.protobuf import timestamp_pb2 as google_dot_protobuf_dot_timestamp__
 from google.protobuf import empty_pb2 as google_dot_protobuf_dot_empty__pb2
 
 
-DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(
-    b'\n\x0cudsink.proto\x12\x07sink.v1\x1a\x1fgoogle/protobuf/timestamp.proto\x1a\x1bgoogle/protobuf/empty.proto";\n\tEventTime\x12.\n\nevent_time\x18\x01 \x01(\x0b\x32\x1a.google.protobuf.Timestamp":\n\tWatermark\x12-\n\twatermark\x18\x01 \x01(\x0b\x32\x1a.google.protobuf.Timestamp"\x86\x01\n\x0c\x44\x61tumRequest\x12\x0c\n\x04keys\x18\x01 \x03(\t\x12\r\n\x05value\x18\x02 \x01(\x0c\x12&\n\nevent_time\x18\x03 \x01(\x0b\x32\x12.sink.v1.EventTime\x12%\n\twatermark\x18\x04 \x01(\x0b\x32\x12.sink.v1.Watermark\x12\n\n\x02id\x18\x05 \x01(\t"\x1e\n\rReadyResponse\x12\r\n\x05ready\x18\x01 \x01(\x08"8\n\x08Response\x12\n\n\x02id\x18\x01 \x01(\t\x12\x0f\n\x07success\x18\x02 \x01(\x08\x12\x0f\n\x07\x65rr_msg\x18\x03 \x01(\t"4\n\x0cResponseList\x12$\n\tresponses\x18\x01 \x03(\x0b\x32\x11.sink.v1.Response2\x86\x01\n\x0fUserDefinedSink\x12\x38\n\x06SinkFn\x12\x15.sink.v1.DatumRequest\x1a\x15.sink.v1.ResponseList(\x01\x12\x39\n\x07IsReady\x12\x16.google.protobuf.Empty\x1a\x16.sink.v1.ReadyResponseb\x06proto3'
-)
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x0cudsink.proto\x12\x07sink.v1\x1a\x1fgoogle/protobuf/timestamp.proto\x1a\x1bgoogle/protobuf/empty.proto\";\n\tEventTime\x12.\n\nevent_time\x18\x01 \x01(\x0b\x32\x1a.google.protobuf.Timestamp\":\n\tWatermark\x12-\n\twatermark\x18\x01 \x01(\x0b\x32\x1a.google.protobuf.Timestamp\"\x86\x01\n\x0c\x44\x61tumRequest\x12\x0c\n\x04keys\x18\x01 \x03(\t\x12\r\n\x05value\x18\x02 \x01(\x0c\x12&\n\nevent_time\x18\x03 \x01(\x0b\x32\x12.sink.v1.EventTime\x12%\n\twatermark\x18\x04 \x01(\x0b\x32\x12.sink.v1.Watermark\x12\n\n\x02id\x18\x05 \x01(\t\"\x1e\n\rReadyResponse\x12\r\n\x05ready\x18\x01 \x01(\x08\"8\n\x08Response\x12\n\n\x02id\x18\x01 \x01(\t\x12\x0f\n\x07success\x18\x02 \x01(\x08\x12\x0f\n\x07\x65rr_msg\x18\x03 \x01(\t\"4\n\x0cResponseList\x12$\n\tresponses\x18\x01 \x03(\x0b\x32\x11.sink.v1.Response2\x86\x01\n\x0fUserDefinedSink\x12\x38\n\x06SinkFn\x12\x15.sink.v1.DatumRequest\x1a\x15.sink.v1.ResponseList(\x01\x12\x39\n\x07IsReady\x12\x16.google.protobuf.Empty\x1a\x16.sink.v1.ReadyResponseb\x06proto3')
 
 _builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, globals())
-_builder.BuildTopDescriptorsAndMessages(DESCRIPTOR, "udsink_pb2", globals())
+_builder.BuildTopDescriptorsAndMessages(DESCRIPTOR, 'udsink_pb2', globals())
 if _descriptor._USE_C_DESCRIPTORS == False:
-    DESCRIPTOR._options = None
-    _EVENTTIME._serialized_start = 87
-    _EVENTTIME._serialized_end = 146
-    _WATERMARK._serialized_start = 148
-    _WATERMARK._serialized_end = 206
-    _DATUMREQUEST._serialized_start = 209
-    _DATUMREQUEST._serialized_end = 343
-    _READYRESPONSE._serialized_start = 345
-    _READYRESPONSE._serialized_end = 375
-    _RESPONSE._serialized_start = 377
-    _RESPONSE._serialized_end = 433
-    _RESPONSELIST._serialized_start = 435
-    _RESPONSELIST._serialized_end = 487
-    _USERDEFINEDSINK._serialized_start = 490
-    _USERDEFINEDSINK._serialized_end = 624
+
+  DESCRIPTOR._options = None
+  _EVENTTIME._serialized_start=87
+  _EVENTTIME._serialized_end=146
+  _WATERMARK._serialized_start=148
+  _WATERMARK._serialized_end=206
+  _DATUMREQUEST._serialized_start=209
+  _DATUMREQUEST._serialized_end=343
+  _READYRESPONSE._serialized_start=345
+  _READYRESPONSE._serialized_end=375
+  _RESPONSE._serialized_start=377
+  _RESPONSE._serialized_end=433
+  _RESPONSELIST._serialized_start=435
+  _RESPONSELIST._serialized_end=487
+  _USERDEFINEDSINK._serialized_start=490
+  _USERDEFINEDSINK._serialized_end=624
 # @@protoc_insertion_point(module_scope)

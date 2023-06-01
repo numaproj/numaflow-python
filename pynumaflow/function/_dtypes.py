55f2c701d31e99d493c915b68ea8d029fc464c3d
Avik Basu <ab93@users.noreply.github.com>
2023-06-01 17:15:58 -0400

    chore: add deprecation for items()
    Signed-off-by: Avik Basu <ab93@users.noreply.github.com>

--- pynumaflow/function/_dtypes.py
+++ pynumaflow/function/_dtypes.py
@@ -3,6 +3,7 @@ from collections.abc import Iterator, Sequence
 from dataclasses import dataclass
 from datetime import datetime
 from typing import TypeVar
+from warnings import warn
 
 from pynumaflow.function.asynciter import NonBlockingIterator
 
@@ -91,6 +92,12 @@ class Messages(Sequence[M]):
         self._messages.append(message)
 
     def items(self) -> list[Message]:
+        warn(
+            "Using items is deprecated and will be removed in v0.5. "
+            "Iterate or index the Messages object instead.",
+            DeprecationWarning,
+            stacklevel=2,
+        )
         return self._messages
 
 
@@ -181,6 +188,12 @@ class MessageTs(Sequence[MT]):
         self._message_ts.append(message_t)
 
     def items(self) -> list[MessageT]:
+        warn(
+            "Using items is deprecated and will be removed in v0.5. "
+            "Iterate or index the Messages object instead.",
+            DeprecationWarning,
+            stacklevel=2,
+        )
         return self._message_ts
 
 

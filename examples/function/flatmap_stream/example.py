fa47683f112defec8ab30ad15b457694577e252a
Hao Hao <xdevxhao@gmail.com>
2023-06-01 14:41:17 -0700

    Update to use async server
    Signed-off-by: Hao Hao <xdevxhao@gmail.com>

--- examples/function/flatmap_stream/example.py
+++ examples/function/flatmap_stream/example.py
@@ -1,14 +1,15 @@
+import aiorun
 from collections.abc import AsyncIterable
 
-from pynumaflow.function import Messages, Message, Datum, Server
+from pynumaflow.function import Message, Datum, AsyncServer
 
 
-async def my_handler(keys: list[str], datum: Datum) -> AsyncIterable[Message]:
+async def map_stream_handler(_: list[str], datum: Datum) -> AsyncIterable[Message]:
     val = datum.value
     _ = datum.event_time
     _ = datum.watermark
     strs = val.decode("utf-8").split(",")
-    Messages()
+
     if len(strs) == 0:
         yield Message.to_drop()
         return
@@ -17,5 +18,5 @@ async def my_handler(keys: list[str], datum: Datum) -> AsyncIterable[Message]:
 
 
 if __name__ == "__main__":
-    grpc_server = Server(map_handler=my_handler)
-    grpc_server.start()
+    grpc_server = AsyncServer(map_stream_handler=map_stream_handler)
+    aiorun.run(grpc_server.start())

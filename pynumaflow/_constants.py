42bc1acab78b5e803400982d6f0ba21d10a5ee27
Derek Wang <whynowy@gmail.com>
2023-07-07 15:28:52 -0700

    chore: set default max gRPC msg size to 64 MB (#101)
    Signed-off-by: Derek Wang <whynowy@gmail.com>

--- pynumaflow/_constants.py
+++ pynumaflow/_constants.py
@@ -7,7 +7,7 @@ SINK_SOCK_PATH = "/var/run/numaflow/udsink.sock"
 # https://github.com/numaproj/numaflow-go/blob/main/pkg/function/configs.go#L6
 WIN_START_TIME = "x-numaflow-win-start-time"
 WIN_END_TIME = "x-numaflow-win-end-time"
-MAX_MESSAGE_SIZE = 1024 * 1024 * 4
+MAX_MESSAGE_SIZE = 1024 * 1024 * 64
 # TODO: None instead of "EOF" ?
 STREAM_EOF = "EOF"
 DELIMITER = ":"

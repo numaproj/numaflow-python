1ba673dc961f07836a166f2df886b8805db98f62
Sidhant Kohli <sidhant.kohli@gmail.com>
2024-01-08 17:09:42 -0800

    sink
    Signed-off-by: Sidhant Kohli <sidhant.kohli@gmail.com>

--- pynumaflow/mapper/__init__.py
+++ pynumaflow/mapper/__init__.py
@@ -3,12 +3,4 @@ from pynumaflow.mapper.map import MapServer
 from pynumaflow.mapper._dtypes import Message, Messages, Datum, DROP, MapperClass
 from pynumaflow._constants import ServerType
 
-__all__ = [
-    "Message",
-    "Messages",
-    "Datum",
-    "DROP",
-    "ServerType",
-    "MapperClass",
-    "MapServer"
-]
+__all__ = ["Message", "Messages", "Datum", "DROP", "ServerType", "MapperClass", "MapServer"]

b0d95422d9af046558d309019d0b539ac3d59b90
Sidhant Kohli <sidhant.kohli@gmail.com>
2024-04-17 14:15:26 -0700

    comment
    Signed-off-by: Sidhant Kohli <sidhant.kohli@gmail.com>

--- pynumaflow/sinker/servicer/utils.py
+++ pynumaflow/sinker/servicer/utils.py
@@ -3,10 +3,10 @@ from pynumaflow.sinker._dtypes import Response
 
 
 def build_sink_response(rspn: Response):
-    if rspn.fallback:
-        return sink_pb2.SinkResponse.Result(id=rspn.id, status=sink_pb2.Status.FALLBACK)
-    elif rspn.success:
+    if rspn.success:
         return sink_pb2.SinkResponse.Result(id=rspn.id, status=sink_pb2.Status.SUCCESS)
+    elif rspn.fallback:
+        return sink_pb2.SinkResponse.Result(id=rspn.id, status=sink_pb2.Status.FALLBACK)
     else:
         return sink_pb2.SinkResponse.Result(
             id=rspn.id, status=sink_pb2.Status.FAILURE, err_msg=rspn.err

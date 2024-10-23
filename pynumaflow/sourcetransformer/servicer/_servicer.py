a1d3eb5bf5323cd483462a7a4bae981bbade6791
Sidhant Kohli <sidhant.kohli@gmail.com>
2024-10-22 21:02:21 -0700

    feat: streaming transformer (#196)
    Signed-off-by: Sidhant Kohli <sidhant.kohli@gmail.com>

---
+++ pynumaflow/sourcetransformer/servicer/_servicer.py
@@ -0,0 +1,150 @@
+import threading
+from concurrent.futures import ThreadPoolExecutor
+from collections.abc import Iterable
+
+from google.protobuf import empty_pb2 as _empty_pb2
+from google.protobuf import timestamp_pb2 as _timestamp_pb2
+
+from pynumaflow.shared.server import exit_on_error
+from pynumaflow.shared.synciter import SyncIterator
+from pynumaflow.sourcetransformer import Datum
+from pynumaflow.sourcetransformer._dtypes import SourceTransformCallable
+from pynumaflow.proto.sourcetransformer import transform_pb2
+from pynumaflow.proto.sourcetransformer import transform_pb2_grpc
+from pynumaflow.types import NumaflowServicerContext
+from pynumaflow._constants import _LOGGER, STREAM_EOF, NUM_THREADS_DEFAULT
+
+
+def _create_read_handshake_response() -> transform_pb2.SourceTransformResponse:
+    """
+    Create a handshake response for the SourceTransform function.
+
+    Returns:
+    SourceTransformResponse: A SourceTransformResponse object indicating a successful handshake.
+    """
+    return transform_pb2.SourceTransformResponse(
+        handshake=transform_pb2.Handshake(sot=True),
+    )
+
+
+class SourceTransformServicer(transform_pb2_grpc.SourceTransformServicer):
+    """
+    This class is used to create a new grpc SourceTransform servicer instance.
+    It implements the SourceTransformServicer interface from the proto transform.proto file.
+    Provides the functionality for the required rpc methods.
+    """
+
+    def __init__(self, handler: SourceTransformCallable, multiproc: bool = False):
+        self.__transform_handler: SourceTransformCallable = handler
+        # This indicates whether the grpc server attached is multiproc or not
+        self.multiproc = multiproc
+        # create a thread pool for executing UDF code
+        self.executor = ThreadPoolExecutor(max_workers=NUM_THREADS_DEFAULT)
+
+    def SourceTransformFn(
+        self,
+        request_iterator: Iterable[transform_pb2.SourceTransformRequest],
+        context: NumaflowServicerContext,
+    ) -> Iterable[transform_pb2.SourceTransformResponse]:
+        """
+        Applies a function to each datum element.
+        The pascal case function name comes from the generated transform_pb2_grpc.py file.
+        """
+        try:
+            # The first message to be received should be a valid handshake
+            req = next(request_iterator)
+            # check if it is a valid handshake req
+            if not (req.handshake and req.handshake.sot):
+                raise Exception("SourceTransformFn: expected handshake message")
+            yield _create_read_handshake_response()
+
+            # result queue to stream messages from the user code back to the client
+            result_queue = SyncIterator()
+
+            # Reader thread to keep reading from the request iterator and schedule
+            # execution for each of them
+            reader_thread = threading.Thread(
+                target=self._process_requests, args=(context, request_iterator, result_queue)
+            )
+            reader_thread.start()
+
+            # Read the result queue and keep forwarding them upstream
+            for res in result_queue.read_iterator():
+                # if error handler accordingly
+                if isinstance(res, BaseException):
+                    # Terminate the current server process due to exception
+                    exit_on_error(context, repr(res), parent=self.multiproc)
+                    return
+                # return the result
+                yield res
+
+            # wait for the threads to clean-up
+            reader_thread.join()
+            self.executor.shutdown(cancel_futures=True)
+
+        except BaseException as err:
+            _LOGGER.critical("UDFError, re-raising the error", exc_info=True)
+            # Terminate the current server process due to exception
+            exit_on_error(context, repr(err), parent=self.multiproc)
+            return
+
+    def _process_requests(
+        self,
+        context: NumaflowServicerContext,
+        request_iterator: Iterable[transform_pb2.SourceTransformRequest],
+        result_queue: SyncIterator,
+    ):
+        try:
+            # read through all incoming requests and submit to the
+            # threadpool for invocation
+            for request in request_iterator:
+                _ = self.executor.submit(self._invoke_transformer, context, request, result_queue)
+            # wait for all tasks to finish after all requests exhausted
+            self.executor.shutdown(wait=True)
+            # Indicate to the result queue that no more messages left to process
+            result_queue.put(STREAM_EOF)
+        except BaseException as e:
+            _LOGGER.critical("SourceTransformFnError, re-raising the error", exc_info=True)
+            result_queue.put(e)
+
+    def _invoke_transformer(
+        self, context, request: transform_pb2.SourceTransformRequest, result_queue: SyncIterator
+    ):
+        try:
+            d = Datum(
+                keys=list(request.request.keys),
+                value=request.request.value,
+                event_time=request.request.event_time.ToDatetime(),
+                watermark=request.request.watermark.ToDatetime(),
+                headers=dict(request.request.headers),
+            )
+            responses = self.__transform_handler(list(request.request.keys), d)
+
+            results = []
+            for resp in responses:
+                event_time_timestamp = _timestamp_pb2.Timestamp()
+                event_time_timestamp.FromDatetime(dt=resp.event_time)
+                results.append(
+                    transform_pb2.SourceTransformResponse.Result(
+                        keys=list(resp.keys),
+                        value=resp.value,
+                        tags=resp.tags,
+                        event_time=event_time_timestamp,
+                    )
+                )
+            result_queue.put(
+                transform_pb2.SourceTransformResponse(results=results, id=request.request.id)
+            )
+        except BaseException as e:
+            _LOGGER.critical("SourceTransform handler error", exc_info=True)
+            result_queue.put(e)
+            return
+
+    def IsReady(
+        self, request: _empty_pb2.Empty, context: NumaflowServicerContext
+    ) -> transform_pb2.ReadyResponse:
+        """
+        IsReady is the heartbeat endpoint for gRPC.
+        The pascal case function name comes from the proto transform_pb2_grpc.py file.
+        """
+        return transform_pb2.ReadyResponse(ready=True)
--- tests/sourcetransform/test_sync_server.py
+++ tests/sourcetransform/test_sync_server.py
@@ -7,15 +7,12 @@ from google.protobuf import timestamp_pb2 as _timestamp_pb2
 from grpc import StatusCode
 from grpc_testing import server_from_dictionary, strict_real_time
 
-from pynumaflow.sourcetransformer import SourceTransformServer
 from pynumaflow.proto.sourcetransformer import transform_pb2
-from tests.sourcetransform.utils import transform_handler, err_transform_handler
+from pynumaflow.sourcetransformer import SourceTransformServer
+from tests.sourcetransform.utils import transform_handler, err_transform_handler, get_test_datums
 from tests.testing_utils import (
-    mock_event_time,
-    mock_watermark,
-    mock_message,
-    mock_new_event_time,
     mock_terminate_on_stop,
+    mock_new_event_time,
 )
 
 
@@ -43,30 +40,33 @@ class TestServer(unittest.TestCase):
         services = {transform_pb2.DESCRIPTOR.services_by_name["SourceTransform"]: my_servicer}
         self.test_server = server_from_dictionary(services, strict_real_time())
 
-        event_time_timestamp = _timestamp_pb2.Timestamp()
-        event_time_timestamp.FromDatetime(dt=mock_event_time())
-        watermark_timestamp = _timestamp_pb2.Timestamp()
-        watermark_timestamp.FromDatetime(dt=mock_watermark())
+        test_datums = get_test_datums()
 
-        request = transform_pb2.SourceTransformRequest(
-            value=mock_message(),
-            event_time=event_time_timestamp,
-            watermark=watermark_timestamp,
-        )
-
-        method = self.test_server.invoke_unary_unary(
+        method = self.test_server.invoke_stream_stream(
             method_descriptor=(
                 transform_pb2.DESCRIPTOR.services_by_name["SourceTransform"].methods_by_name[
                     "SourceTransformFn"
                 ]
             ),
-            invocation_metadata={
-                ("this_metadata_will_be_skipped", "test_ignore"),
-            },
-            request=request,
+            invocation_metadata={},
             timeout=1,
         )
-        response, metadata, code, details = method.termination()
+
+        for x in test_datums:
+            method.send_request(x)
+        method.requests_closed()
+
+        responses = []
+        while True:
+            try:
+                resp = method.take_response()
+                responses.append(resp)
+            except ValueError as err:
+                if "No more responses!" in err.__str__():
+                    break
+
+        metadata, code, details = method.termination()
+        self.assertTrue("Something is fishy" in details)
         self.assertEqual(grpc.StatusCode.UNKNOWN, code)
 
     def test_is_ready(self):
@@ -86,51 +86,93 @@ class TestServer(unittest.TestCase):
         self.assertEqual(expected, response)
         self.assertEqual(code, StatusCode.OK)
 
-    def test_mapt_assign_new_event_time(self, test_server=None):
-        event_time_timestamp = _timestamp_pb2.Timestamp()
-        event_time_timestamp.FromDatetime(dt=mock_event_time())
-        watermark_timestamp = _timestamp_pb2.Timestamp()
-        watermark_timestamp.FromDatetime(dt=mock_watermark())
+    def test_udf_mapt_err_handshake(self):
+        server = SourceTransformServer(source_transform_instance=err_transform_handler)
+        my_servicer = server.servicer
+        services = {transform_pb2.DESCRIPTOR.services_by_name["SourceTransform"]: my_servicer}
+        self.test_server = server_from_dictionary(services, strict_real_time())
 
-        request = transform_pb2.SourceTransformRequest(
-            keys=["test"],
-            value=mock_message(),
-            event_time=event_time_timestamp,
-            watermark=watermark_timestamp,
-        )
-        serv = self.test_server
-        if test_server:
-            serv = test_server
-
-        method = serv.invoke_unary_unary(
+        test_datums = get_test_datums(handshake=False)
+        method = self.test_server.invoke_stream_stream(
             method_descriptor=(
                 transform_pb2.DESCRIPTOR.services_by_name["SourceTransform"].methods_by_name[
                     "SourceTransformFn"
                 ]
             ),
-            invocation_metadata={
-                ("this_metadata_will_be_skipped", "test_ignore"),
-            },
-            request=request,
+            invocation_metadata={},
             timeout=1,
         )
 
-        response, metadata, code, details = method.termination()
-        self.assertEqual(1, len(response.results))
-        self.assertEqual(["test"], response.results[0].keys)
-        self.assertEqual(
-            bytes(
-                "payload:test_mock_message " "event_time:2022-09-12 16:00:00 ",
-                encoding="utf-8",
+        for x in test_datums:
+            method.send_request(x)
+        method.requests_closed()
+
+        responses = []
+        while True:
+            try:
+                resp = method.take_response()
+                responses.append(resp)
+            except ValueError as err:
+                if "No more responses!" in err.__str__():
+                    break
+
+        metadata, code, details = method.termination()
+        self.assertTrue("SourceTransformFn: expected handshake message" in details)
+        self.assertEqual(grpc.StatusCode.UNKNOWN, code)
+
+    def test_mapt_assign_new_event_time(self):
+        test_datums = get_test_datums()
+
+        method = self.test_server.invoke_stream_stream(
+            method_descriptor=(
+                transform_pb2.DESCRIPTOR.services_by_name["SourceTransform"].methods_by_name[
+                    "SourceTransformFn"
+                ]
             ),
-            response.results[0].value,
+            invocation_metadata={},
+            timeout=1,
         )
+
+        for x in test_datums:
+            method.send_request(x)
+        method.requests_closed()
+
+        responses = []
+        while True:
+            try:
+                resp = method.take_response()
+                responses.append(resp)
+            except ValueError as err:
+                if "No more responses!" in err.__str__():
+                    break
+
+        metadata, code, details = method.termination()
+
+        # 1 handshake + 3 data responses
+        self.assertEqual(4, len(responses))
+
+        self.assertTrue(responses[0].handshake.sot)
+
+        idx = 1
+        while idx < len(responses):
+            _id = "test-id-" + str(idx)
+            self.assertEqual(_id, responses[idx].id)
+            self.assertEqual(
+                bytes(
+                    "payload:test_mock_message " "event_time:2022-09-12 16:00:00 ",
+                    encoding="utf-8",
+                ),
+                responses[idx].results[0].value,
+            )
+            self.assertEqual(1, len(responses[idx].results))
+            idx += 1
+
         # Verify new event time gets assigned.
         updated_event_time_timestamp = _timestamp_pb2.Timestamp()
         updated_event_time_timestamp.FromDatetime(dt=mock_new_event_time())
         self.assertEqual(
             updated_event_time_timestamp,
-            response.results[0].event_time,
+            responses[1].results[0].event_time,
         )
         self.assertEqual(code, StatusCode.OK)
 

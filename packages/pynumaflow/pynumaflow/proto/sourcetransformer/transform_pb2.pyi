49bb7fbc180f196450c5bd4f5e2bdee648eefb4f
Sreekanth <prsreekanth920@gmail.com>
2025-10-20 09:34:39 +0530

    include metadata in sourcetransformer request/response
    Signed-off-by: Sreekanth <prsreekanth920@gmail.com>

--- packages/pynumaflow/pynumaflow/proto/sourcetransformer/transform_pb2.pyi
+++ packages/pynumaflow/pynumaflow/proto/sourcetransformer/transform_pb2.pyi
@@ -2,6 +2,7 @@ import datetime
 
 from google.protobuf import timestamp_pb2 as _timestamp_pb2
 from google.protobuf import empty_pb2 as _empty_pb2
+from pynumaflow.proto.common import metadata_pb2 as _metadata_pb2
 from google.protobuf.internal import containers as _containers
 from google.protobuf import descriptor as _descriptor
 from google.protobuf import message as _message
@@ -19,7 +20,7 @@ class Handshake(_message.Message):
 class SourceTransformRequest(_message.Message):
     __slots__ = ("request", "handshake")
     class Request(_message.Message):
-        __slots__ = ("keys", "value", "event_time", "watermark", "headers", "id")
+        __slots__ = ("keys", "value", "event_time", "watermark", "headers", "id", "metadata")
         class HeadersEntry(_message.Message):
             __slots__ = ("key", "value")
             KEY_FIELD_NUMBER: _ClassVar[int]
@@ -33,13 +34,15 @@ class SourceTransformRequest(_message.Message):
         WATERMARK_FIELD_NUMBER: _ClassVar[int]
         HEADERS_FIELD_NUMBER: _ClassVar[int]
         ID_FIELD_NUMBER: _ClassVar[int]
+        METADATA_FIELD_NUMBER: _ClassVar[int]
         keys: _containers.RepeatedScalarFieldContainer[str]
         value: bytes
         event_time: _timestamp_pb2.Timestamp
         watermark: _timestamp_pb2.Timestamp
         headers: _containers.ScalarMap[str, str]
         id: str
-        def __init__(self, keys: _Optional[_Iterable[str]] = ..., value: _Optional[bytes] = ..., event_time: _Optional[_Union[datetime.datetime, _timestamp_pb2.Timestamp, _Mapping]] = ..., watermark: _Optional[_Union[datetime.datetime, _timestamp_pb2.Timestamp, _Mapping]] = ..., headers: _Optional[_Mapping[str, str]] = ..., id: _Optional[str] = ...) -> None: ...
+        metadata: _metadata_pb2.Metadata
+        def __init__(self, keys: _Optional[_Iterable[str]] = ..., value: _Optional[bytes] = ..., event_time: _Optional[_Union[datetime.datetime, _timestamp_pb2.Timestamp, _Mapping]] = ..., watermark: _Optional[_Union[datetime.datetime, _timestamp_pb2.Timestamp, _Mapping]] = ..., headers: _Optional[_Mapping[str, str]] = ..., id: _Optional[str] = ..., metadata: _Optional[_Union[_metadata_pb2.Metadata, _Mapping]] = ...) -> None: ...
     REQUEST_FIELD_NUMBER: _ClassVar[int]
     HANDSHAKE_FIELD_NUMBER: _ClassVar[int]
     request: SourceTransformRequest.Request
@@ -49,16 +52,18 @@ class SourceTransformRequest(_message.Message):
 class SourceTransformResponse(_message.Message):
     __slots__ = ("results", "id", "handshake")
     class Result(_message.Message):
-        __slots__ = ("keys", "value", "event_time", "tags")
+        __slots__ = ("keys", "value", "event_time", "tags", "metadata")
         KEYS_FIELD_NUMBER: _ClassVar[int]
         VALUE_FIELD_NUMBER: _ClassVar[int]
         EVENT_TIME_FIELD_NUMBER: _ClassVar[int]
         TAGS_FIELD_NUMBER: _ClassVar[int]
+        METADATA_FIELD_NUMBER: _ClassVar[int]
         keys: _containers.RepeatedScalarFieldContainer[str]
         value: bytes
         event_time: _timestamp_pb2.Timestamp
         tags: _containers.RepeatedScalarFieldContainer[str]
-        def __init__(self, keys: _Optional[_Iterable[str]] = ..., value: _Optional[bytes] = ..., event_time: _Optional[_Union[datetime.datetime, _timestamp_pb2.Timestamp, _Mapping]] = ..., tags: _Optional[_Iterable[str]] = ...) -> None: ...
+        metadata: _metadata_pb2.Metadata
+        def __init__(self, keys: _Optional[_Iterable[str]] = ..., value: _Optional[bytes] = ..., event_time: _Optional[_Union[datetime.datetime, _timestamp_pb2.Timestamp, _Mapping]] = ..., tags: _Optional[_Iterable[str]] = ..., metadata: _Optional[_Union[_metadata_pb2.Metadata, _Mapping]] = ...) -> None: ...
     RESULTS_FIELD_NUMBER: _ClassVar[int]
     ID_FIELD_NUMBER: _ClassVar[int]
     HANDSHAKE_FIELD_NUMBER: _ClassVar[int]

9f74b6ecf30acb25731ea1d451be95f4cbc437b6
Sreekanth <prsreekanth920@gmail.com>
2026-03-06 18:15:22 +0530

    Rewrite tests with pytest
    Signed-off-by: Sreekanth <prsreekanth920@gmail.com>

--- packages/pynumaflow/tests/map/test_sync_mapper.py
+++ packages/pynumaflow/tests/map/test_sync_mapper.py
@@ -1,7 +1,5 @@
-import unittest
-from unittest.mock import patch
-
 import grpc
+import pytest
 from google.protobuf import empty_pb2 as _empty_pb2
 from grpc import StatusCode
 from grpc_testing import server_from_dictionary, strict_real_time
@@ -9,88 +7,72 @@ from grpc_testing import server_from_dictionary, strict_real_time
 from pynumaflow.mapper import MapServer
 from pynumaflow.proto.mapper import map_pb2
 from tests.map.utils import map_handler, err_map_handler, ExampleMap, get_test_datums
-from tests.testing_utils import (
-    mock_terminate_on_stop,
-)
 
 
-# We are mocking the terminate function from the psutil to not exit the program during testing
-@patch("psutil.Process.kill", mock_terminate_on_stop)
-class TestSyncMapper(unittest.TestCase):
-    # @patch("psutil.Process.kill", mock_terminate_on_stop)
-    def setUp(self) -> None:
-        class_instance = ExampleMap()
-        my_server = MapServer(mapper_instance=class_instance)
-        services = {map_pb2.DESCRIPTOR.services_by_name["Map"]: my_server.servicer}
-        self.test_server = server_from_dictionary(services, strict_real_time())
+@pytest.fixture
+def test_server():
+    class_instance = ExampleMap()
+    my_server = MapServer(mapper_instance=class_instance)
+    services = {map_pb2.DESCRIPTOR.services_by_name["Map"]: my_server.servicer}
+    return server_from_dictionary(services, strict_real_time())
 
-    def test_init_with_args(self) -> None:
+
+def _invoke_map(test_server, handshake=True):
+    test_datums = get_test_datums(handshake=handshake)
+    method = test_server.invoke_stream_stream(
+        method_descriptor=(map_pb2.DESCRIPTOR.services_by_name["Map"].methods_by_name["MapFn"]),
+        invocation_metadata={},
+        timeout=1,
+    )
+    for x in test_datums:
+        method.send_request(x)
+    method.requests_closed()
+
+    responses = []
+    while True:
+        try:
+            resp = method.take_response()
+            responses.append(resp)
+        except ValueError as err:
+            if "No more responses!" in str(err):
+                break
+    return method, responses
+
+
+def _err_server():
+    my_server = MapServer(mapper_instance=err_map_handler)
+    services = {map_pb2.DESCRIPTOR.services_by_name["Map"]: my_server.servicer}
+    return server_from_dictionary(services, strict_real_time())
+
+
+class TestSyncMapper:
+    def test_init_with_args(self):
         my_servicer = MapServer(
             mapper_instance=map_handler,
             sock_path="/tmp/test.sock",
             max_message_size=1024 * 1024 * 5,
         )
-        self.assertEqual(my_servicer.sock_path, "unix:///tmp/test.sock")
-        self.assertEqual(my_servicer.max_message_size, 1024 * 1024 * 5)
+        assert my_servicer.sock_path == "unix:///tmp/test.sock"
+        assert my_servicer.max_message_size == 1024 * 1024 * 5
 
     def test_udf_map_err_handshake(self):
-        my_server = MapServer(mapper_instance=err_map_handler)
-        services = {map_pb2.DESCRIPTOR.services_by_name["Map"]: my_server.servicer}
-        self.test_server = server_from_dictionary(services, strict_real_time())
-
-        test_datums = get_test_datums(handshake=False)
-        method = self.test_server.invoke_stream_stream(
-            method_descriptor=(map_pb2.DESCRIPTOR.services_by_name["Map"].methods_by_name["MapFn"]),
-            invocation_metadata={},
-            timeout=1,
-        )
-        for x in test_datums:
-            method.send_request(x)
-        method.requests_closed()
-
-        responses = []
-        while True:
-            try:
-                resp = method.take_response()
-                responses.append(resp)
-            except ValueError as err:
-                if "No more responses!" in err.__str__():
-                    break
+        server = _err_server()
+        method, responses = _invoke_map(server, handshake=False)
 
         metadata, code, details = method.termination()
-        self.assertTrue("MapFn: expected handshake as the first message" in details)
-        self.assertEqual(grpc.StatusCode.INTERNAL, code)
+        assert "MapFn: expected handshake as the first message" in details
+        assert code == grpc.StatusCode.INTERNAL
 
     def test_udf_map_error_response(self):
-        my_server = MapServer(mapper_instance=err_map_handler)
-        services = {map_pb2.DESCRIPTOR.services_by_name["Map"]: my_server.servicer}
-        self.test_server = server_from_dictionary(services, strict_real_time())
-
-        test_datums = get_test_datums(handshake=True)
-        method = self.test_server.invoke_stream_stream(
-            method_descriptor=(map_pb2.DESCRIPTOR.services_by_name["Map"].methods_by_name["MapFn"]),
-            invocation_metadata={},
-            timeout=1,
-        )
-        for x in test_datums:
-            method.send_request(x)
-        method.requests_closed()
-
-        responses = []
-        while True:
-            try:
-                resp = method.take_response()
-                responses.append(resp)
-            except ValueError as err:
-                if "No more responses!" in err.__str__():
-                    break
+        server = _err_server()
+        method, responses = _invoke_map(server, handshake=True)
 
         metadata, code, details = method.termination()
-        self.assertTrue("Something is fishy!" in details)
-        self.assertEqual(grpc.StatusCode.INTERNAL, code)
+        assert "Something is fishy!" in details
+        assert code == grpc.StatusCode.INTERNAL
 
-    def test_is_ready(self):
-        method = self.test_server.invoke_unary_unary(
+    def test_is_ready(self, test_server):
+        method = test_server.invoke_unary_unary(
             method_descriptor=(
                 map_pb2.DESCRIPTOR.services_by_name["Map"].methods_by_name["IsReady"]
             ),
@@ -100,70 +82,42 @@ class TestSyncMapper(unittest.TestCase):
         )
 
         response, metadata, code, details = method.termination()
-        expected = map_pb2.ReadyResponse(ready=True)
-        self.assertEqual(expected, response)
-        self.assertEqual(code, StatusCode.OK)
+        assert response == map_pb2.ReadyResponse(ready=True)
+        assert code == StatusCode.OK
 
-    def test_map_forward_message(self):
-        test_datums = get_test_datums(handshake=True)
-        method = self.test_server.invoke_stream_stream(
-            method_descriptor=(map_pb2.DESCRIPTOR.services_by_name["Map"].methods_by_name["MapFn"]),
-            invocation_metadata={},
-            timeout=1,
-        )
-        for x in test_datums:
-            method.send_request(x)
-        method.requests_closed()
-
-        responses = []
-        while True:
-            try:
-                resp = method.take_response()
-                responses.append(resp)
-            except ValueError as err:
-                if "No more responses!" in err.__str__():
-                    break
+    def test_map_forward_message(self, test_server):
+        method, responses = _invoke_map(test_server, handshake=True)
 
         metadata, code, details = method.termination()
         # 1 handshake + 3 data responses
-        self.assertEqual(4, len(responses))
-
-        self.assertTrue(responses[0].handshake.sot)
+        assert len(responses) == 4
+        assert responses[0].handshake.sot
 
         result_ids = {f"test-id-{id}" for id in range(1, 4)}
-        idx = 1
-        while idx < len(responses):
-            result_ids.remove(responses[idx].id)
-            self.assertEqual(
-                bytes(
-                    "payload:test_mock_message "
-                    "event_time:2022-09-12 16:00:00 watermark:2022-09-12 16:01:00",
-                    encoding="utf-8",
-                ),
-                responses[idx].results[0].value,
+        for resp in responses[1:]:
+            result_ids.remove(resp.id)
+            assert resp.results[0].value == bytes(
+                "payload:test_mock_message "
+                "event_time:2022-09-12 16:00:00 watermark:2022-09-12 16:01:00",
+                encoding="utf-8",
             )
-            self.assertEqual(1, len(responses[idx].results))
-            idx += 1
-        self.assertEqual(len(result_ids), 0)
-        self.assertEqual(code, StatusCode.OK)
+            assert len(resp.results) == 1
+        assert len(result_ids) == 0
+        assert code == StatusCode.OK
 
     def test_invalid_input(self):
-        with self.assertRaises(TypeError):
+        with pytest.raises(TypeError):
             MapServer()
 
     def test_max_threads(self):
         # max cap at 16
         server = MapServer(mapper_instance=map_handler, max_threads=32)
-        self.assertEqual(server.max_threads, 16)
+        assert server.max_threads == 16
 
         # use argument provided
         server = MapServer(mapper_instance=map_handler, max_threads=5)
-        self.assertEqual(server.max_threads, 5)
+        assert server.max_threads == 5
 
         # defaults to 4
         server = MapServer(mapper_instance=map_handler)
-        self.assertEqual(server.max_threads, 4)
-
-
-if __name__ == "__main__":
-    unittest.main()
+        assert server.max_threads == 4
